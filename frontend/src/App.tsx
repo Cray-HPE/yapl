3721a46ede28415a49d0d20282bcc7844bc4dda6
Di Zou <di.zou@hpe.com>
2021-08-09 11:24:41 -0400

    fix layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { connect } from './api/ws';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "./api/ws";
+import { Col, Layout, Menu, Row } from "antd";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
+import ReactMarkdown from "react-markdown";
 
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
 class App extends Component {
   constructor(props: any) {
     super(props);
@@ -10,22 +19,158 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <Layout style={{ height: "100vh" }}>
+        <Header style={{ background: "#01A982", height: "8vh" }}>Header</Header>
+        <Content style={{ padding: "5vh 2vw" }}>
+          <Layout
+            className="site-layout-background"
+            style={{ padding: "0", height: "100%" }}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <Row style={{ margin: 0 }}>
+              <Col
+                flex={3}
+                style={{
+                  background: "#24292f",
+                  height: "82vh",
+                  overflow: "auto",
+                  color: "white"
+                }}
+              >
+                <div
+                  style={{ margin: "1vh", height: "80vh", overflow: "auto"  }}
+                >
+                  ...
+                  <br />
+                  Really
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  long
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  ...
+                  <br />
+                  content
+                </div>
+              </Col>
+              <Col
+                flex={2}
+                style={{ margin: "1vh", height: "80vh", overflow: "auto" }}
+              >
+                <ReactMarkdown>
+                  {`# Hello, *world*!
+    
+## sadfsadf
+## sadfsadf    
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+## sadfsadf
+
+`}
+                </ReactMarkdown>
+              </Col>
+            </Row>
+          </Layout>
+        </Content>
+      </Layout>
     );
   }
 }
