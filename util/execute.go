9948afa8c9145819eb2700c2beae8a2459f41f58
Di Zou <di.zou@hpe.com>
2021-10-28 12:02:20 -0400

    all console output and log to a separate file

--- util/execute.go
+++ util/execute.go
@@ -59,19 +59,19 @@ func executeStep(pipeline *model.GenericYAML) error {
 		fmt.Println()
 		pterm.Debug.Println(MarkdownToText(pipeline.Metadata.Description))
 
-		err := execute(job.PreCondition, "Step: "+pipeline.Metadata.Name+" --- Checking Precondition")
+		err := execute(job.PreCondition, "Step: "+pipeline.Metadata.Name+" --- Checking Precondition --- "+pipeline.Metadata.Id)
 		PushToCache(step.ToGeneric()) //nolint
 		if err != nil {
 			return err
 		}
 
-		err = execute(job.Action, "Step: "+pipeline.Metadata.Name+" --- Executing Action")
+		err = execute(job.Action, "Step: "+pipeline.Metadata.Name+" --- Executing Action --- "+pipeline.Metadata.Id)
 		PushToCache(step.ToGeneric()) //nolint
 		if err != nil {
 			return err
 		}
 
-		err = execute(job.PostValidation, "Step: "+pipeline.Metadata.Name+" --- Post action validation")
+		err = execute(job.PostValidation, "Step: "+pipeline.Metadata.Name+" --- Post action validation --- "+pipeline.Metadata.Id)
 		PushToCache(step.ToGeneric()) //nolint
 		if err != nil {
 			return err
@@ -85,23 +85,42 @@ func executeStep(pipeline *model.GenericYAML) error {
 func execute(runnable *model.Runnable, name string) error {
 
 	var outputBuf bytes.Buffer
-	output := bufio.NewWriter(&outputBuf)
+	var output io.Writer
+	if os.Getenv("ConsoleOutput") == "true" {
+		output = io.MultiWriter(os.Stdout, os.Stderr, &outputBuf)
+	} else {
+		output = bufio.NewWriter(&outputBuf)
+	}
 	spinner, _ := pterm.DefaultSpinner.WithShowTimer(true).Start(name)
 
 	pterm.Debug.Println(MarkdownToText(runnable.Description))
 
 	err := runCommand(runnable.Command, output)
 	runnable.Output = outputBuf.String()
+	writeConsoleOutputTofile(outputBuf, name)
 	if err != nil {
 		spinner.Fail()
-		fmt.Println(runnable.Output)
+		if os.Getenv("ConsoleOutput") != "true" {
+			fmt.Println(runnable.Output)
+		}
 		pterm.FgRed.Println("Check the doc below for troubleshooting:")
 		fmt.Println(MarkdownToText(runnable.Troubleshooting))
 		return err
 	}
 	spinner.Success()
-	if os.Getenv("ConsoleOutput") == "true" {
-		fmt.Println(runnable.Output)
+	return nil
+}
+
+func writeConsoleOutputTofile(outputBuf bytes.Buffer, name string) error {
+	// write console log to file
+	f, err := os.OpenFile("yapl.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.WriteString("==> " + name + "\n" + outputBuf.String() + "\n\n"); err != nil {
+		return err
 	}
 	return nil
 }
